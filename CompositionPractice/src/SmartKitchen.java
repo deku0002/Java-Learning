02c19812129851191041ccd0f65d9267d423db2c
Devansh Sharma <121448392+deku0002@users.noreply.github.com>
2024-12-21 11:35:59 +0530

    Composition Practice
    Tried the compostiton by self creating a class SmartKitchen which has a (has a relationship) with the classes regrigerator, dishwasher, coffee maker. we can access all the components for smart kitchen rather than creating object of each class in the main and make it more private and safer.

---
+++ CompositionPractice/src/SmartKitchen.java
@@ -0,0 +1,36 @@
+public class SmartKitchen {
+
+    private CoffeeMaker brewMaster;
+    private DishWasher dishWasher;
+    private Refrigerator iceBox;
+
+    public SmartKitchen(){
+        brewMaster = new CoffeeMaker();
+        dishWasher = new DishWasher();
+        iceBox = new Refrigerator();
+    }
+
+    public void setKitchenState(boolean coffeeFlag,boolean fridgeFlag,boolean dishWasherFlag){
+        brewMaster.setHasWorkToDo(coffeeFlag);
+        iceBox.setHasWorkToDo(fridgeFlag);
+        dishWasher.setHasWorkToDo(dishWasherFlag);
+    }
+
+    public void doKitchenWork(){
+        brewMaster.brewCoffee();
+        iceBox.orderFood();
+        dishWasher.doDishes();
+    }
+
+    public CoffeeMaker getBrewMaster() {
+        return brewMaster;
+    }
+
+    public DishWasher getDishWasher() {
+        return dishWasher;
+    }
+
+    public Refrigerator getIceBox() {
+        return iceBox;
+    }
+}
