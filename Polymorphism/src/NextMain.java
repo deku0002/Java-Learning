0d4eaced80378304cb45bdfded1c0ca9aabe68fd
Devansh Sharma <121448392+deku0002@users.noreply.github.com>
2024-12-22 17:45:13 +0530

    Add files via upload

---
+++ Polymorphism/src/NextMain.java
@@ -0,0 +1,19 @@
+public class NextMain {
+    public static void main(String[] args) {
+        Movie themovie = Movie.getMovie("A","Jaws");
+        themovie.watchMovie();
+
+        Adventure jaws = (Adventure) Movie.getMovie("A","Jaws");
+        jaws.watchMovie();
+
+        Object comedy = Movie.getMovie("C","PK");
+        Comedy comedyMovie = (Comedy) comedy;
+        comedyMovie.watchComedy();
+
+        var airplane = Movie.getMovie("C","Airplane");
+        airplane.watchMovie();
+
+        var plane = new Comedy("Airplane");
+        plane.watchComedy();
+    }
+}
